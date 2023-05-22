e6f1d1f628fb1d502b2cf7511613df27e2641643
Tayco Murilo <tmsr@ic.ufal.br>
2023-05-22 19:04:04 -0300

    funcionando
    Signed-off-by: Tayco Murilo <tmsr@ic.ufal.br>

---
+++ micro_model/main/input.h
@@ -0,0 +1,7 @@
+
+#ifndef INPUT_H_
+#define INPUT_H_
+
+extern unsigned char bem_te_vi[];
+
+#endif  // INPUT_H_
--- micro_model/main/main_functions.h
+++ micro_model/main/main_functions.h
@@ -1,41 +1,31 @@
-/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+#ifndef MAIN_FUNCTIONS_H_
+#define MAIN_FUNCTIONS_H_
 
-    http://www.apache.org/licenses/LICENSE-2.0
+// #include "tensorflow/lite/micro/all_ops_resolver.h"
+#include <tensorflow/lite/micro/micro_mutable_op_resolver.h>
+#include <tensorflow/lite/micro/micro_interpreter.h>
+#include <tensorflow/lite/micro/system_setup.h>
+#include <tensorflow/lite/schema/schema_generated.h>
+#include <tensorflow/lite/micro/micro_log.h>
+#include <tensorflow/lite/c/common.h>
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-#ifndef TENSORFLOW_LITE_MICRO_EXAMPLES_HELLO_WORLD_MAIN_FUNCTIONS_H_
-#define TENSORFLOW_LITE_MICRO_EXAMPLES_HELLO_WORLD_MAIN_FUNCTIONS_H_
-
-#include "tensorflow/lite/micro/all_ops_resolver.h"
-#include "tensorflow/lite/micro/micro_interpreter.h"
-#include "tensorflow/lite/micro/system_setup.h"
-#include "tensorflow/lite/schema/schema_generated.h"
+// #include <freertos/FreeRTOS.h>
+// #include <freertos/task.h">
 // Expose a C friendly interface for main functions.
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// Initializes all data needed for the example. The name is important, and needs
-// to be setup() for Arduino compatibility.
 void setup();
 
-// Runs one iteration of data gathering and inference. This should be called
-// repeatedly from the application code. The name needs to be loop() for Arduino
-// compatibility.
 void loop();
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif  // TENSORFLOW_LITE_MICRO_EXAMPLES_HELLO_WORLD_MAIN_FUNCTIONS_H_
+#endif  // MAIN_FUNCTIONS_H_
